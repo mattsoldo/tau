50abede678367502aa29bb4729f3e3fa8a294211
Matthew Soldo <matt@soldo.org>
2026-01-31 22:35:30 -0800

    Release v1.6.7

--- daemon/src/tau/main.py
+++ daemon/src/tau/main.py
@@ -47,7 +47,7 @@ class TauDaemon:
 
     async def startup(self):
         """Initialize all daemon components"""
-        logger.info("tau_daemon_starting", version="1.6.5")
+        logger.info("tau_daemon_starting", version="1.6.7")
 
         # Initialize database
         logger.info("initializing_database")
