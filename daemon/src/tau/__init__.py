e13606308edfad5d880999ca17d34d84966ad732
Matthew Soldo <matt@soldo.org>
2026-02-01 00:00:31 -0800

    Release v1.7.1

--- daemon/src/tau/__init__.py
+++ daemon/src/tau/__init__.py
@@ -4,5 +4,5 @@ Tau Lighting Control Daemon
 A professional-grade smart lighting control system for residential and commercial environments.
 """
 
-__version__ = "1.7.0"
+__version__ = "1.7.1"
 __author__ = "Tau Development Team"
